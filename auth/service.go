0063dc94a787f704661e642aecedd4983b99aaaa
hilmiikhsan <robbinhood22012@gmail.com>
2022-12-29 11:37:00 +0700

    add validation jwt token

--- auth/service.go
+++ auth/service.go
@@ -1,9 +1,14 @@
 package auth
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"errors"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
 
 type jwtService struct {
@@ -19,11 +24,27 @@ func (s *jwtService) GenerateToken(userID int) (string, error) {
 	claims := jwt.MapClaims{}
 	claims["user_id"] = userID
 
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	signedToken, err := token.SignedString(SECRET_KEY)
 	if err != nil {
 		return signedToken, err
 	}
 
 	return signedToken, nil
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("Invalid Token")
+		}
+
+		return []byte(SECRET_KEY), nil
+	})
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
 }
